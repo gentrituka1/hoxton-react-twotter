ce5dad1d2171e43c00a57fb72079f7158e3890d9
Gentrit Uka <gentrit-uka@live.com>
2022-08-08 18:42:41 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,73 @@
-import './App.css'
-import { Link } from 'react-router-dom'
-import {FaBeer, BiHomeCircle} from 'react-icons/fa'
+import "./App.css";
+import { Link } from "react-router-dom";
+import { FaBeer, BiHomeCircle } from "react-icons/fa";
 
 function App() {
-
   return (
     <div className="App">
-     <section className="left-menu">
-          <ul className='left-menu__list'>
-              <li>
-                <Link to={'/home'}>
-                  <img className='svg' src="../twittersvg.svg" alt="home"/>
-                </Link>
-              </li>
-              <li>
-                <div className='left-menu__icons'>
-                  <span><BiHomeCircle /></span>
-                  <h2>Home</h2>
-                </div>
-              </li>
-              <li>
-                <div className='left-menu__icons'>
-                  <img src="/" alt="Explore" />
-                  <h2>Explore</h2>
-                </div>
-              </li>
-              <li>
-                <div className='left-menu__icons'>
-                    <img src="/" alt="Notifications" />
-                    <h2>Notifications</h2>
-                </div>
-              </li>
-              <li>
-                <div className='left-menu__icons'>
-                    <img src="/" alt="Messages" />
-                    <h2>Messages</h2>
-                </div>
-              </li>
-              <li>
-                <div className='left-menu__icons'>
-                  <img src="/" alt="Bookmarks" />
-                  <h2>Bookmarks</h2>
-                </div>
-              </li>
-              <li>
-                <div className='left-menu__icons'>
-                  <img src="/" alt="Lists" />
-                  <h2>Lists</h2>
-                </div>
-              </li>
-              <li>
-                <div className='left-menu__icons'>
-                  <img src="/" alt="Profile" />
-                  <h2>Profile</h2>
-                </div>
-              </li>
-              <li>
-                <div className='left-menu__icons'>
-                  <img src="/" alt="More" />
-                  <h2>More</h2>
-                </div>
-              </li>
-              <li>
-                <div className='left-menu__icons'>
-                  <button>Tweet</button>
-                </div>
-              </li>
-          </ul>
+      <section className="left-menu">
+        <ul className="left-menu__list">
+          <li>
+            <Link to={"/home"}>
+              <img className="svg" src="../twittersvg.svg" alt="home" />
+            </Link>
+          </li>
+          <li>
+            <div className="left-menu__icons">
+              <span>
+                <BiHomeCircle />
+              </span>
+              <h2>Home</h2>
+            </div>
+          </li>
+          <li>
+            <div className="left-menu__icons">
+              <img src="/" alt="Explore" />
+              <h2>Explore</h2>
+            </div>
+          </li>
+          <li>
+            <div className="left-menu__icons">
+              <img src="/" alt="Notifications" />
+              <h2>Notifications</h2>
+            </div>
+          </li>
+          <li>
+            <div className="left-menu__icons">
+              <img src="/" alt="Messages" />
+              <h2>Messages</h2>
+            </div>
+          </li>
+          <li>
+            <div className="left-menu__icons">
+              <img src="/" alt="Bookmarks" />
+              <h2>Bookmarks</h2>
+            </div>
+          </li>
+          <li>
+            <div className="left-menu__icons">
+              <img src="/" alt="Lists" />
+              <h2>Lists</h2>
+            </div>
+          </li>
+          <li>
+            <div className="left-menu__icons">
+              <img src="/" alt="Profile" />
+              <h2>Profile</h2>
+            </div>
+          </li>
+          <li>
+            <div className="left-menu__icons">
+              <img src="/" alt="More" />
+              <h2>More</h2>
+            </div>
+          </li>
+          <li>
+            <div className="left-menu__icons">
+              <button>Tweet</button>
+            </div>
+          </li>
+        </ul>
       </section>
       <main className="main-menu">
         <h2>Main Content</h2>
@@ -75,7 +76,7 @@ function App() {
         <h2>Right Menu</h2>
       </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
